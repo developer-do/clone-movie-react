e2e6cac27aeb236afed64aed213ad5a7bca280df
developer-do <dev-do@naver.com>
2020-07-04 00:06:15 +0900

    05-1 state로 숫자 증감기능 만들어보기

--- clone_movie/src/App.js
+++ clone_movie/src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 
 class App extends React.Component {
   // React.Component 클래스의 기능을 추가한 App 클래스
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.state.count++;
+    console.log(this.state.count);
+  };
+
+  minus = () => {
+    this.state.count--;
+    console.log(this.state.count);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
