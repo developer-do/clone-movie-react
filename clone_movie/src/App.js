620e0d6bd921b47ca3a748e88dc3499d4b394258
developer-do <dev-do@naver.com>
2020-07-11 11:44:13 +0900

    앱 배포! url - clone-react.dev-do.co.kr

--- clone_movie/src/App.js
+++ clone_movie/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 import Navigation from './components/Navigation';
 
 function App() {
+  console.log(BrowserRouter);
+  console.log(HashRouter);
+  console.log(Route);
   return (
-    <HashRouter>
+    <BrowserRouter >
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
       <Route path="/movie-detail" component={Detail} />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
